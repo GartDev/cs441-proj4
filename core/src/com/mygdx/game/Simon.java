98c304a07b2f734caa6f278c4c846690d97b2c5a
GartDev <gartdev@Garrett.localdomain>
2019-03-07 20:30:07 -0500

    Began to render visuals and gather additional assets

--- core/src/com/mygdx/game/Simon.java
+++ core/src/com/mygdx/game/Simon.java
@@ -4,30 +4,53 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Simon extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture dogtex;
+	private Texture cattex;
+	private Texture cowtex;
+	private Texture goattex;
+	private Sprite dogsprite;
+	private Sprite catsprite;
+	private Sprite cowsprite;
+	private Sprite goatsprite;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		dogtex = new Texture("dog.bmp");
+		cattex = new Texture("cat.bmp");
+		cowtex = new Texture("cow.bmp");
+		goattex = new Texture("goat.bmp");
+		dogsprite = new Sprite(dogtex);
+		catsprite = new Sprite(cattex);
+		cowsprite = new Sprite(cowtex);
+		goatsprite = new Sprite(goattex);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		dogsprite.draw(batch);
+		catsprite.draw(batch);
+		cowsprite.draw(batch);
+		goatsprite.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		dogtex.dispose();
+		cattex.dispose();
+		cowtex.dispose();
+		goattex.dispose();
 	}
 }
